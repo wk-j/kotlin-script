4c3e2270d33ff36539673bca89a2bba9a80b7dcb
wk-j <somnuk.wk@gmail.com>
2017-07-30 23:56:15 +0700

    Compile to JavaScript

---
+++ Scripts/Library.kt
@@ -0,0 +1,9 @@
+// kotlinc-js -output Scripts/Library.js -meta-info Scripts/Library.kt
+// https://kotlinlang.org/docs/tutorials/javascript/getting-started-command-line/command-line-library-js.html
+
+fun factorial(n: Int) : Long =
+    if (n == 0) 1 else n * factorial (n - 1)
+
+inline fun IntRange.forOdd(f: (Int) -> Unit) { 
+    this.forEach { if (it %2 == 1) f(it) }
+}
